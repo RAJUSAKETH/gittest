b94722942294afefc8bc00bdb2b3dfb74d262aa3
Raju <raju.rud@gmail.com>
2014-03-08 10:52:07 -0500

    Test 2

--- gittest/src/HelloWorld.java
+++ gittest/src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hare Krishna!!");
-		System.out.println("Test");
+		System.out.println("Hello World!!");
 	}
 
 }
